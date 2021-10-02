87c06714c13c4a6ae26bc6cb32d635680223d8b6
kevd3r <kevin.derot@gmail.com>
2021-10-02 23:31:59 +0200

    added js working js functions and 1rst css bootstrap

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,24 @@
-console.log('bonjour');
+let hold = 0;
+let rollDice = 0;
+
+let newGame = document.getElementById('newGame');
+newGame.addEventListener('click', ()=>{
+  console.log('match');
+  hold=0;
+  rolldice=0;
+})
+
 function rollingDice(){
   return Math.floor(Math.random()*6)+1;
 }
 
-let rollDice = rollingDice();
+console.log(hold);
 console.log(rollDice);
-let rollDiceBtn = document.getElementById('rolldice');
-let result = document.getElementById('result');
-  rollDiceBtn.addEventListener('click',()=>{
-        result.innerText = rollingDice();
-        console.log('match');
-    })
+let resultRollDice = document.getElementById('result-rolldice');
+let holdBtn = document.getElementById('rollDice');
+holdBtn.addEventListener('click', ()=>{
+  console.log('rematch');
+  hold=rollingDice();
+  console.log(hold);
+  resultRollDice.textContent = hold;
+})
