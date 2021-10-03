1154aae94b9b661adfc0a260082cdefcd04dcc2f
kevd3r <kevin.derot@gmail.com>
2021-10-03 16:04:38 +0200

    added new functions to dispatch scores

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,49 @@
 let hold = 0;
 let rollDice = 0;
+let playerOne;
+let playerTwo;
+let globalOne;
+let globalTwo;
 
+
+/**----Initializing New Game ------- */
 let newGame = document.getElementById('newGame');
 newGame.addEventListener('click', ()=>{
   console.log('match');
   hold=0;
   rolldice=0;
+  globalOne=0;
+  globalTwo=0;
+  playerOne=true;
 })
 
+/**------Roll Dice Function------ */
 function rollingDice(){
   return Math.floor(Math.random()*6)+1;
 }
-
-console.log(hold);
-console.log(rollDice);
+/**------Aquiring result of the rolldice----- */
 let resultRollDice = document.getElementById('result-rolldice');
-let holdBtn = document.getElementById('rollDice');
-holdBtn.addEventListener('click', ()=>{
+let rollBtn = document.getElementById('rollDice');
+rollBtn.addEventListener('click', ()=>{
   console.log('rematch');
   hold=rollingDice();
   console.log(hold);
   resultRollDice.textContent = hold;
+})
+
+/**------counting global score ----------------*/
+let holdBtn = document.getElementById('hold');
+let result1 = document.getElementById('result1');
+let result2 = document.getElementById('result2');
+holdBtn.addEventListener('click',()=>{
+  if (playerOne==true){
+    globalOne= globalOne + hold;
+    console.log(globalOne);
+    result1.textContent = globalOne;
+  }
+  if(playerOne!==true){
+    globalTwo=globalTwo + hold;
+    console.log(globalTwo);
+    result2.textContent = globalTwo;
+  }
 })
